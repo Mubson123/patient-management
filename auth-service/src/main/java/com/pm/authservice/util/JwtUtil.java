735f03b2af092ece392822f4ec86cdc9cacff8ad
Mubson123 <cedricmube@gmail.com>
2025-09-26 11:19:38 +0200

    generate jwt done

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -3,19 +3,19 @@ package com.pm.authservice.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
-@Component
+@Service
 public class JwtUtil {
 
   private final Key secretKey;
 
-  public JwtUtil(@Value("${jwt.secretKey}") String secret) {
+  public JwtUtil(@Value("${jwt.secret}") String secret) {
     byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8));
     this.secretKey = Keys.hmacShaKeyFor(keyBytes);
   }
