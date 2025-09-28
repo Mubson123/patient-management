dd202302a9f44fd453938ca83ad03a795d93dcce
Mubson123 <cedricmube@gmail.com>
2025-09-28 20:16:47 +0200

    get-all-patients unit test done

--- patient-service/src/main/java/com/pm/patientservice/model/Address.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Address.java
@@ -2,13 +2,11 @@ package com.pm.patientservice.model;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -3,10 +3,7 @@ package com.pm.patientservice.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Patient {
