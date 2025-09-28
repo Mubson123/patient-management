1c1811dbbe9355cc1b395d0b919b6c3152665961
Mubson123 <cedricmube@gmail.com>
2025-09-28 21:02:19 +0200

    create-patient unit-test done

--- patient-service/src/test/java/service/PatientServiceTest.java
+++ patient-service/src/test/java/service/PatientServiceTest.java
@@ -1,6 +1,9 @@
 package service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -16,11 +19,17 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PatientServiceTest {
 
+    @Mock
+    private BillingServiceGrpcClient billingServiceGrpcClient;
+    @Mock
+    private KafkaProducer kafkaProducer;
     @Mock
     private PatientMapper patientMapper;
     @Mock
@@ -34,6 +43,7 @@ class PatientServiceTest {
         List<PatientResponseDTO> expected = PatientFixtures.patientResponseDTOS;
         when(patientRepository.findAll()).thenReturn(patientList);
         when(patientMapper.toDTOList(patientList)).thenReturn(expected);
+
         List<PatientResponseDTO> actual = unitUnderTest.getAllPatients();
         assertEquals(expected, actual);
     }
@@ -44,7 +54,24 @@ class PatientServiceTest {
         PatientResponseDTO expected = PatientFixtures.sarahResponse;
         when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
         when(patientMapper.toDTO(patient)).thenReturn(expected);
+
         PatientResponseDTO actual = unitUnderTest.getPatientById(patient.getId());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCreatePatient() {
+        Patient patient = PatientFixtures.john;
+        PatientRequestDTO patientRequestDTO = PatientFixtures.johnRequest;
+        PatientResponseDTO expected = PatientFixtures.johnResponse;
+        when(patientRepository.existsByEmail(patientRequestDTO.getEmail())).thenReturn(false);
+        when(patientMapper.toEntity(patientRequestDTO)).thenReturn(patient);
+        when(patientRepository.save(patient)).thenReturn(patient);
+        doNothing().when(billingServiceGrpcClient).createBillingAccount(anyString(), anyString(), anyString(), anyString());
+        doNothing().when(kafkaProducer).sendEvent(patient);
+        when(patientMapper.toDTO(patient)).thenReturn(expected);
+
+        PatientResponseDTO actual = unitUnderTest.createPatient(patientRequestDTO);
         assertEquals(expected, actual);
     }
 }
